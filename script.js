018257ecd958a51a927017f9d8dcf5c76e3caf0a
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-06-02 11:31:34 -0400

    ...

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 ///Vue Instance - an object VueJS provides us with
 
 ////Control either the whole / certain part of our instance
+
+console.log("Detected");
 new Vue({
   /// String - says which element on the page this is gonna control
   el: "#vue-app", ///Control the div with id = vue-app
 
-  /// Stores the data for this Vue Instance [el] -- Obkect
+  /// Stores the data for this Vue Instance [el] -- Object
   data: {
-    name: Shawn,
+    name: "Shawn",
   },
 });
