10067b36d6ccc76eaa943f451159c4d1ebeea277
Carol-Student22 <Carol-Student22@users.noreply.github.com>
2021-07-03 11:51:29 -0400

    Video #7 Complete

---
+++ KeyboardEventScript.js
@@ -0,0 +1,12 @@
+new Vue ({
+    el: '#vue-app',
+    data: {},
+    methods:{
+        logName: function () {
+            console.log("You've entered your name");
+        },
+        logAge: function () {
+            console.log("You've entered your age");
+        }
+    },
+})
